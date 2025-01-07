4a4144b661c765da48975b5ea5d931e405611274
seelamGayatri2006 <seelamgayatri18@gmail.com>
2025-01-07 15:35:16 +0000

    It is a dictionary

---
+++ Day1.py
@@ -0,0 +1,8 @@
+my_integer = 42
+my_float = 3.14159
+my_string = "Hello, world!"
+my_boolean = True
+print(f"Integer: {my_integer}, Type: {type(my_integer)}")
+print(f"Float: {my_float}, Type: {type(my_float)}")
+print(f"String: {my_string}, Type: {type(my_string)}")
+print(f"Boolean: {my_boolean}, Type: {type(my_boolean)}")
---
+++ Day2.py
@@ -0,0 +1,32 @@
+my_list = [10, 20, 30, 40, 50]
+first_element = my_list[0]   
+third_element = my_list[2]  
+last_element = my_list[-1]   
+print(f"First Element: {first_element}")
+print(f"Third Element: {third_element}")
+print(f"Last Element: {last_element}")
+
+
+
+my_tuple = ('apple', 'banana', 'cherry', 'date', 'elderberry')
+second_element = my_tuple[1]   
+fourth_element = my_tuple[3]   
+last_element_tuple = my_tuple[-1] 
+print(f"Second Element: {second_element}")
+print(f"Fourth Element: {fourth_element}")
+print(f"Last Element: {last_element_tuple}")
+
+
+my_dict = {
+    'name': 'Alice',
+    'age': 30,
+    'city': 'New York',
+    'job': 'Engineer',
+    'is_student': False
+}
+name = my_dict['name']   
+age = my_dict['age']     
+job_status = my_dict['job']  
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Job: {job_status}")
